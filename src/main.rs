b7f1c9c114238f3c9be9cf0dc2c485093bd8d2c4
Gly <gabin.lefranc@gmail.com>
2023-04-14 15:40:26 +0200

    Working example with openai connection

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,15 @@
 mod openai;
 // mod http2;
 use std::{
-    net::TcpStream, 
     io::{
-        Read, 
         Write
     }, str::FromStr
 };
-use serde::{Serialize, Deserialize, de::Error};
+
 use tokio_stream::StreamExt;
-use tokio::io::AsyncRead;
 
-fn main() {
-    let stream = std::fs::read_to_string("example.txt").expect("Failed to read file");
-    let result = stream
-        .split("\n\n")
-        .filter(|item| !item.is_empty())
-        .map(openai::ChatResponse::from_str)
-        .filter_map(|item| {
-            match item {
-                Ok(item) => Some(item),
-                Err(e) => {
-                    if cfg!(debug_assertions) {
-                        println!("Error: {:?}", e); 
-                    }
-                    None
-                },
-            }
-        })
-        .for_each(|item| print!("{}", item));
-
-
-}
-// #[tokio::main]
-async fn main2() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
     let openai_api_key = std::env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set");
     // Send a request
     let chat_request = openai::ChatRequest::new("gpt-3.5-turbo".to_string())
@@ -51,15 +27,33 @@ async fn main2() -> std::io::Result<()> {
         .expect("Failed to send request")
         .bytes_stream();
     
-    // while let Some(item) = stream.next()
-    //     .map(|item| item.ok())
-    //     .flatten()
-    //     .map(|item| String::from_utf8_lossy(item.as_ref())
-    //         .split("\n\n")
-    //         .map(|item| String::from(item))
-    //         .map(|item| serde_json::from_str::<openai::ChatResponse>(&item))
-    //         .collect::<Vec<_>>()) {
-    //         println!("Chunk: {:?}", item)
-    // }
+    while let Some(item) = stream.next().await
+        .map(Result::ok)
+        .flatten() {
+            // println!("stream received");
+            String::from_utf8_lossy(item.as_ref())
+                .split("\n\n")
+                .filter(|item| !item.is_empty())
+                .map(openai::ChatResponse::from_str)
+                .filter_map(|item| {
+                    match item {
+                        Ok(item) => Some(item),
+                        Err(e) => {
+                            if cfg!(debug_assertions) {
+                                println!("Error: {:?}", e); 
+                            }
+                            None
+                        },
+                    }
+                })
+                .for_each(|item| {
+                    print!("{}", item);
+                    if let Err(e) = std::io::stdout().flush() {
+                        if cfg!(debug_assertions) {
+                            println!("Error: {:?}", e); 
+                        }
+                    };
+                })
+    }
     Ok(())
 }
