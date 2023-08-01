9d065cea302a74eae17bccbbe652f51874160d3b
Gly <gabin.lefranc@gmail.com>
2023-08-01 18:51:43 +0200

    add analyzer system

--- src/markdown/parser.rs
+++ src/markdown/parser.rs
@@ -12,20 +12,28 @@ impl<E> From<E> for ParseError<E> {
 }
 
 pub struct Parser<R: Renderer> {
-    pub renderer: R,
+    renderer: R,
+    mode_func: fn(&mut Self, char) -> Result<(), ParseError<R::Error>>,
 }
 impl<R: Renderer> Parser<R> {
     // type Result<T> = Result<T, ParseError<R::Error>>;
     pub fn new(renderer: R) -> Self {
         Self {
-            renderer
+            renderer,
+            mode_func: Self::analyse_text,
         }
     }
     pub fn push(&mut self, text: &str) -> Result<(), ParseError<R::Error>> {
         for c in text.chars() {
-            self.analyse_char(c)?;
+            (self.mode_func)(self, c)?;
         }
         Ok(self.renderer.flush()?)
     }
-    
+    pub fn analyse_text(&mut self, c: char) -> Result<(), ParseError<R::Error>> {
+        self.mode_func = Self::analyse_text;
+        Ok(())
+    }
+    pub fn end_of_document(&mut self) -> Result<(), ParseError<R::Error>> {
+        todo!("End of document");
+    }
 }
