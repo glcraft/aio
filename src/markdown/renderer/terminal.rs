43bd639ed6e403c46ed387c863712623f23cd4bc
Gly <gabin.lefranc@gmail.com>
2023-08-01 18:51:50 +0200

    add TerminalRenderer new

--- src/markdown/renderer/terminal.rs
+++ src/markdown/renderer/terminal.rs
@@ -3,6 +3,12 @@ use super::Renderer;
 
 pub struct TerminalRenderer;
 
+impl TerminalRenderer {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
 impl Renderer for TerminalRenderer {
     type Error = ();
     fn push_token(&mut self, _style: token::Token) -> Result<(), Self::Error> {
