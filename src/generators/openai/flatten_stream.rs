bfb2efbfcff2e6666605ae3104e65df6e6f19f39
Gly <gabin.lefranc@gmail.com>
2023-08-19 00:50:56 +0200

    WIP flatten takes iterator instof stream

--- src/generators/openai/flatten_stream.rs
+++ src/generators/openai/flatten_stream.rs
@@ -1,31 +1,12 @@
 use tokio_stream::Stream;
 
-trait FlattenTrait: Stream + Sized 
-where 
-    <Self as Stream>::Item: Stream + Unpin,
-    <<Self as Stream>::Item as Stream>::Item: Stream + Unpin,
+pub trait FlattenTrait: Stream
 {
     // type Item: Stream;
-    fn flatten_stream(self) -> Flatten<Self>;
-}
-
-struct Flatten<I> 
-where
-    I: Stream,
-    <I as Stream>::Item: Stream + Unpin,
-    <<I as Stream>::Item as Stream>::Item: Stream + Unpin,
-{
-    stream: I,
-    current: Option<<I as Stream>::Item>
-}
-
-impl<I> FlattenTrait for I 
-where
-    I: Stream,
-    <I as Stream>::Item: Stream + Unpin,
-    <<I as Stream>::Item as Stream>::Item: Stream + Unpin,
-{
-    fn flatten_stream(self) -> Flatten<Self> {
+    fn flatten_stream(self) -> Flatten<Self>
+    where 
+        Self: Sized,
+    {
         Flatten {
             stream: self,
             current: None
@@ -33,25 +14,91 @@ where
     }
 }
 
+pub struct Flatten<I> 
+where
+    I: Stream,
+{
+    stream: I,
+    current: Option<<I as Stream>::Item>
+}
+
+// impl<I> FlattenTrait for I 
+// where
+//     I: Stream,
+//     <I as Stream>::Item: Stream + Unpin,
+//     <<I as Stream>::Item as Stream>::Item: Stream + Unpin,
+// {
+//     fn flatten_stream(self) -> Flatten<Self> {
+//         Flatten {
+//             stream: self,
+//             current: None
+//         }
+//     }
+// }
+
+// impl<I> Stream for Flatten<I>
+// where
+//     I: Stream,
+//     <I as Stream>::Item: Stream + Unpin,
+//     <<I as Stream>::Item as Stream>::Item: Stream + Unpin,
+// {
+//     type Item = <<I as Stream>::Item as Stream>::Item;
+//     fn poll_next(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<Self::Item>> {
+//         use std::task::Poll::*;
+//         use std::pin::pin;
+//         match self.current {
+//             Some(ref mut item) => {
+//                 let item = pin!(item).poll_next(cx);
+//                 match item {
+//                     Ready(None) => {
+//                         self.current = None;
+//                         self.poll_next(cx)
+//                     }
+//                     v => v,
+//                 }
+//             }
+//             None => {
+//                 let item = pin!(self.stream).poll_next(cx);
+//                 match item {
+//                     Ready(Some(item)) => {
+//                         self.current = Some(item);
+//                         self.poll_next(cx)
+//                     }
+//                     Ready(None) => Ready(None),
+//                     Pending => Pending,
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+impl<St: ?Sized> FlattenTrait for St where St: Stream {}
+// impl<St: ?Sized, It> FlattenTrait for St 
+// where 
+//     It: Iterator,
+//     St: Stream<Item = Result<It, super::Error>>,
+// {}
+
+
 impl<I> Stream for Flatten<I>
 where
     I: Stream,
-    <I as Stream>::Item: Stream + Unpin,
-    <<I as Stream>::Item as Stream>::Item: Stream + Unpin,
+    <I as Stream>::Item: Iterator,
 {
-    type Item = <<I as Stream>::Item as Stream>::Item;
-    fn poll_next(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<Self::Item>> {
+    type Item = <<I as Stream>::Item as Iterator>::Item;
+    fn poll_next(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<<Self as Stream>::Item>> {
         use std::task::Poll::*;
         use std::pin::pin;
         match self.current {
             Some(ref mut item) => {
-                let item = pin!(item).poll_next(cx);
+                let item = item.next();
                 match item {
-                    Ready(None) => {
+                    None => {
                         self.current = None;
                         self.poll_next(cx)
                     }
-                    v => v,
+                    v => Ready(v),
                 }
             }
             None => {
