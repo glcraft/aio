b903640743f7e6fbf9e3d2e14f43260228f7ff63
Gly <gabin.lefranc@gmail.com>
2023-08-18 16:11:35 +0200

    Add optional config and credds path to Args

--- src/arguments.rs
+++ src/arguments.rs
@@ -4,6 +4,14 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
+    /// Configuration file
+    /// Default: ~/.aio/config.yaml
+    #[arg(long, default_value_t = String::from("~/.aio/config.yaml"))]
+    pub config_path: String,
+    /// Credentials file
+    /// Default: ~/.aio/creds.yaml
+    #[arg(long, default_value_t = String::from("~/.aio/creds.yaml"))]
+    pub creds_path: String,
     /// Prompt name
     pub prompt: String,
     /// Input
