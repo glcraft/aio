78ce5186def8081c561156594792c7dfc77ccb61
Gly <gabin.lefranc@gmail.com>
2023-08-23 15:35:37 +0200

    better code shape

--- src/main.rs
+++ src/main.rs
@@ -26,22 +26,29 @@ async fn main() -> Result<(), String> {
     let mut md_parser = markdown::Parser::new(term_renderer);
 
     let args = args::Args::parse();
-    let config = raise_str!(config::Config::from_yaml_file(&args.config_path), "Failed to parse config file: {}");
-    let creds = raise_str!(credentials::Credentials::from_yaml_file(&args.creds_path), "Failed to parse credentials file: {}");
+    let config = raise_str!(
+        config::Config::from_yaml_file(&args.config_path),
+        "Failed to parse config file: {}"
+    );
+    let creds = raise_str!(
+        credentials::Credentials::from_yaml_file(&args.creds_path),
+        "Failed to parse credentials file: {}"
+    );
     // let config_openai = config.openai;
-    let mut stream = generators::openai_run(creds.openai, config, args).await.expect("Failed to run openai API");
+    let mut stream = generators::openai_run(creds.openai, config, args)
+        .await
+        .expect("Failed to run openai API");
     // let stream = std::pin::Pin::into_inner(stream);
     loop {
-
         match stream.next().await {
             Some(Ok(token)) => {
                 raise_str!(md_parser.push(&token));
-            },
+            }
             Some(Err(e)) => {
                 eprintln!("{}", e);
-                break
+                break;
             }
-            None => break
+            None => break,
         }
     }
     Ok(())
