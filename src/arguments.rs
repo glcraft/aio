8af24112ae94219af2cc62d680631ce9bbd8c874
Gly <gabin.lefranc@gmail.com>
2023-08-24 02:23:47 +0200

    Update argument docs

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,22 +1,21 @@
 use clap::Parser;
 
-/// Simple program to greet a person
+/// Program to communicate with large language models and AI API 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Configuration file
-    /// Default: ~/.aio/config.yaml
     #[arg(long, default_value_t = String::from("~/.aio/config.yaml"))]
     pub config_path: String,
     /// Credentials file
-    /// Default: ~/.aio/creds.yaml
     #[arg(long, default_value_t = String::from("~/.aio/creds.yaml"))]
     pub creds_path: String,
     /// Engine name
+    /// 
     /// The name can be followed by custom prompt name from the configuration file
-    /// Ex: openai:command
+    /// (ex: openai:command)
     #[arg(long, short)]
     pub engine: String,
-    /// Input
+    /// User text prompt
     pub input: String,
 }
