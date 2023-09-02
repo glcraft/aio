da0ab784be34f782753628487c5e3e377362edd5
Gly <gabin.lefranc@gmail.com>
2023-09-02 19:30:58 +0200

    Add formatter trait

--- src/formatters/mod.rs
+++ src/formatters/mod.rs
@@ -2,6 +2,14 @@ mod markdown;
 
 pub type MarkdownFormatter = markdown::Parser<markdown::TerminalRenderer>;
 
+pub trait Formatter {
+    type Error;
+    fn push(&mut self, text: &str) -> Result<(), Self::Error>;
+    fn end_of_document(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
 pub fn new_markdown_formatter() -> MarkdownFormatter {
     markdown::Parser::new(markdown::TerminalRenderer::new())
 }
