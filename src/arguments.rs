82c9e3e9052808e0ee4a8f57945aa8b119b5956d
Gly <gabin.lefranc@gmail.com>
2023-09-18 15:13:38 +0200

    Add argument do run code

--- src/arguments.rs
+++ src/arguments.rs
@@ -2,6 +2,35 @@ use std::fmt::Display;
 
 use clap::{Parser, ValueEnum};
 
+/// Program to communicate with large language models and AI API 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// Configuration file
+    #[arg(long, default_value_t = String::from("~/.config/aio/config.yaml"))]
+    pub config_path: String,
+    /// Credentials file
+    #[arg(long, default_value_t = String::from("~/.config/aio/creds.yaml"))]
+    pub creds_path: String,
+    /// Engine name
+    /// 
+    /// The name can be followed by custom prompt name from the configuration file
+    /// (ex: openai:command)
+    #[arg(long, short)]
+    pub engine: String,
+    /// Formatter
+    /// 
+    /// Possible values: markdown, raw
+    #[arg(long, short, default_value_t = Default::default())]
+    pub formatter: FormatterChoice,
+    /// Run code block if the language is supported
+    #[arg(long, short, default_value_t = Default::default())]
+    pub run: RunChoice,
+    /// Force to run code 
+    /// User text prompt
+    pub input: Option<String>,
+}
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 pub enum FormatterChoice {
     Markdown,
@@ -28,31 +57,31 @@ impl Display for FormatterChoice {
     }
 }
 
-/// Program to communicate with large language models and AI API 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-pub struct Args {
-    /// Configuration file
-    #[arg(long, default_value_t = String::from("~/.config/aio/config.yaml"))]
-    pub config_path: String,
-    /// Credentials file
-    #[arg(long, default_value_t = String::from("~/.config/aio/creds.yaml"))]
-    pub creds_path: String,
-    /// Engine name
-    /// 
-    /// The name can be followed by custom prompt name from the configuration file
-    /// (ex: openai:command)
-    #[arg(long, short)]
-    pub engine: String,
-    /// Formatter
-    /// 
-    /// Possible values: markdown, raw
-    #[arg(long, short, default_value_t = Default::default())]
-    pub formatter: FormatterChoice,
-    /// User text prompt
-    pub input: Option<String>,
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum RunChoice {
+    /// Doesn't run anything
+    No,
+    /// Ask to run code
+    Ask,
+    /// Run code without asking
+    Force
 }
 
+impl Default for RunChoice {
+    fn default() -> Self {
+        RunChoice::No
+    }
+}
+
+impl Display for RunChoice {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RunChoice::No => write!(f, "no"),
+            RunChoice::Ask => write!(f, "ask"),
+            RunChoice::Force => write!(f, "force"),
+        }
+    }
+}
 pub struct ProcessedArgs {
     pub config_path: String,
     pub creds_path: String,
