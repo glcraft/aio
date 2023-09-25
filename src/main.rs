9029b33c53f313bcdff03b5fa7be83bb8ea9dd97
Gly <gabin.lefranc@gmail.com>
2023-09-25 16:55:15 +0200

    Better configuration file management

--- src/main.rs
+++ src/main.rs
@@ -45,16 +45,37 @@ fn resolve_path(path: &str) -> Cow<str> {
     }
 }
 
-fn get_config<P: AsRef<std::path::Path>>(path: P) -> Result<config::Config, String> {
-    let path = path.as_ref();
+fn get_config_path(path: &std::path::Path) -> Option<Cow<'_, std::path::Path>> {
     if path.exists() {
-        return config::Config::from_yaml_file(path).map_err(|e| e.to_string())
+        return Some(Cow::Borrowed(path.as_ref()))
     }
-    use std::io::Write;
-    let default_config = config::Config::default();
-    let yaml = serde_yaml::to_string(&default_config).map_err(|e| e.to_string())?;
-    std::fs::File::create(path).unwrap().write_all(yaml.as_bytes()).map_err(|e| e.to_string())?;
-    Ok(default_config)
+    let new_extension = match path.extension().map(|e| e.to_str()).flatten() {
+        Some("yml") => "yaml",
+        Some("yaml") => "yml",
+        _ => return None
+    };
+    let new_path = path.with_extension(new_extension);
+    if new_path.exists() {
+        return Some(Cow::Owned(new_path));
+    }
+    None
+}
+
+fn get_config<P: AsRef<std::path::Path>>(path: P) -> Result<config::Config, String> {
+    let found_path = get_config_path(path.as_ref());
+    let config = match found_path {
+        Some(found_path) => {
+            config::Config::from_yaml_file(found_path).map_err(|e| e.to_string())?
+        }
+        None => {
+            use std::io::Write;
+            let default_config = config::Config::default();
+            let yaml = serde_yaml::to_string(&default_config).map_err(|e| e.to_string())?;
+            std::fs::File::create(path).unwrap().write_all(yaml.as_bytes()).map_err(|e| e.to_string())?;
+            default_config
+        }
+    };
+    Ok(config)
 }
 
 #[tokio::main]
@@ -71,7 +92,7 @@ async fn main() -> Result<(), String> {
         }
         args::ProcessedArgs::from(args)
     };
-    let config = get_config(resolve_path(&args.config_path).as_ref())?;
+    let config = get_config(resolve_path(&args.config_path).as_ref()).map_err(|e| format!("An error occured while loading or creating configuration file: {}", e))?;
     let creds = raise_str!(
         credentials::Credentials::from_yaml_file(resolve_path(&args.creds_path).as_ref()),
         "Failed to parse credentials file: {}"
