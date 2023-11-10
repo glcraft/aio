e598892df73402bda68f1c4d342e64c8a4c3a3c6
Gly <gabin.lefranc@gmail.com>
2023-11-10 16:19:27 +0100

    Format main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 pub mod arguments;
-mod runner;
-mod generators;
-mod formatters;
 mod config;
 mod credentials;
-mod serde_io;
 mod filesystem;
+mod formatters;
+mod generators;
+mod runner;
+mod serde_io;
 
+mod openai {}
+
+use arguments as args;
 use clap::Parser;
+use formatters::Formatter;
 use serde_io::DeserializeExt;
 use tokio_stream::StreamExt;
-use arguments as args;
-use formatters::Formatter;
 
 macro_rules! raise_str {
     ($expr:expr) => {
         raise_str!($expr, "{}")
     };
-    ($expr:expr, $text:literal) => {
-        { $expr.map_err(|e| format!($text, e))? }
-    };
+    ($expr:expr, $text:literal) => {{
+        $expr.map_err(|e| format!($text, e))?
+    }};
 }
 
 #[tokio::main]
@@ -30,15 +32,26 @@ async fn main() -> Result<(), String> {
             use std::io::Read;
             let mut str_input = std::string::String::new();
             let mut stdin = std::io::stdin();
-            stdin.read_to_string(&mut str_input).map_err(|e| format!("Failed to read input from stdin: {}", e))?;
-            
+            stdin
+                .read_to_string(&mut str_input)
+                .map_err(|e| format!("Failed to read input from stdin: {}", e))?;
+
             args.input = Some(str_input.trim().to_string());
         }
         args::ProcessedArgs::from(args)
     };
-    let config = config::Config::from_yaml_file(filesystem::resolve_path(&args.config_path).as_ref()).map_err(|e| format!("An error occured while loading or creating configuration file: {}", e))?;
+    let config =
+        config::Config::from_yaml_file(filesystem::resolve_path(&args.config_path).as_ref())
+            .map_err(|e| {
+                format!(
+                    "An error occured while loading or creating configuration file: {}",
+                    e
+                )
+            })?;
     let creds = raise_str!(
-        credentials::Credentials::from_yaml_file(filesystem::resolve_path(&args.creds_path).as_ref()),
+        credentials::Credentials::from_yaml_file(
+            filesystem::resolve_path(&args.creds_path).as_ref()
+        ),
         "Failed to parse credentials file: {}"
     );
 
@@ -48,23 +61,28 @@ async fn main() -> Result<(), String> {
     };
     let mut runner = runner::Runner::new(args.run);
 
-    let (engine, _prompt) = args.engine
+    let (engine, _prompt) = args
+        .engine
         .find(':')
-        .map(|i| (&args.engine[..i], Some(&args.engine[i+1..])))
+        .map(|i| (&args.engine[..i], Some(&args.engine[i + 1..])))
         .unwrap_or((args.engine.as_str(), None));
 
     let mut stream = match engine {
         "openai" => generators::openai::run(creds.openai, config, args).await,
         "from-file" => generators::debug::run(config, args).await,
         _ => panic!("Unknown engine: {}", engine),
-    }.map_err(|e| format!("Failed to request OpenAI API: {}", e))?;
+    }
+    .map_err(|e| format!("Failed to request OpenAI API: {}", e))?;
 
     loop {
         match stream.next().await {
             Some(Ok(token)) => {
                 raise_str!(formatter.push(&token), "Failed to parse markdown: {}");
-                raise_str!(runner.push(&token), "Failed push text in the runner system: {}");
-            },
+                raise_str!(
+                    runner.push(&token),
+                    "Failed push text in the runner system: {}"
+                );
+            }
             Some(Err(e)) => Err(e.to_string())?,
             None => break,
         }
@@ -73,3 +91,4 @@ async fn main() -> Result<(), String> {
     raise_str!(runner.end_of_document(), "Failed to run code: {}");
     Ok(())
 }
+
