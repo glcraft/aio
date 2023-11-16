71aaefe37df8de1e54c8d3fc9f64dc624c45cf8e
Gly <gabin.lefranc@gmail.com>
2023-11-16 16:58:10 +0100

    Fix split bytes factory

--- src/utils/split_bytes.rs
+++ src/utils/split_bytes.rs
@@ -26,13 +26,7 @@ where
             .position(|b| b == self.separator.as_ref())
             .map(|i| i + sep_len);
 
-        unsafe {
-            println!("total bytes: {}", String::from_utf8_unchecked(bytes.to_vec()));
-        }
-
-        // si none, la trame n'est pas complete
-        // ne 
-        return match pos_last_separator_found {
+        let complete = match pos_last_separator_found {
             Some(pos_last_separator) => {
                 let pos_last_separator = bytes.len() - pos_last_separator;
                 unsafe {
@@ -42,30 +36,14 @@ where
                 std::mem::swap(&mut current, &mut self.rest);
                 current.append(&mut bytes.slice(..pos_last_separator).to_vec());
                 self.rest = bytes.slice((pos_last_separator + sep_len).min(bytes.len())..).to_vec();
-                SplitBytes::new(bytes.slice(..pos_last_separator), self.separator.clone())
+                Bytes::from(current)
             }
             None => {
                 self.rest.append(&mut bytes.to_vec());
-                SplitBytes::new(Bytes::new(), self.separator.clone())
+                Bytes::new()
             }
         };
-
-        if let Some(pos_last_separator) = pos_last_separator_found { 
-            let mut current = Vec::new();
-            std::mem::swap(&mut current, &mut self.rest);
-            current.append(&mut bytes.slice(..pos_last_separator).to_vec());
-            self.rest = bytes.slice((pos_last_separator + sep_len).min(bytes.len())..).to_vec();
-            return SplitBytes::new(bytes.slice(..pos_last_separator), self.separator.clone());
-        }
-        let pos_last_separator = bytes.len() - pos_last_separator_found.unwrap_or(0);
-
-        
-        
-        let mut current = Vec::new();
-        std::mem::swap(&mut current, &mut self.rest);
-        current.append(&mut bytes.slice(..pos_last_separator).to_vec());
-        self.rest = bytes.slice((pos_last_separator + sep_len).min(bytes.len())..).to_vec();
-        SplitBytes::new(Bytes::from(current), self.separator.clone())
+        SplitBytes::new(complete, self.separator.clone())
     }
 }
 
