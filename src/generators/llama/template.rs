627ed7f183b5f5f2fac826e501cc0907476e7c88
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-04-28 15:37:57 +0200

    add custom template

--- src/generators/llama/template.rs
+++ src/generators/llama/template.rs
@@ -3,12 +3,22 @@ use super::super::openai::{Message, Role};
 use llama_cpp::{LlamaTokenizationError, Token};
 
 #[derive(Default, Debug, Deserialize, Serialize)]
+pub struct CustomTemplate {
+    pub system_prefix: String,
+    pub system_suffix: String,
+    pub user_prefix: String,
+    pub user_suffix: String,
+    pub assistant_prefix: String,
+    pub assistant_suffix: String,
+}
+#[derive(Default, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "lowercase")]
 pub enum PromptTemplate {
     #[default]
     ChatML,
     Llama2,
     Llama3,
+    Custom(CustomTemplate)
 }
 
 fn append_to_vec<T: Copy>(vec: &mut Vec<T>, other: &[T]) {
@@ -30,6 +40,7 @@ impl PromptTemplate {
             PromptTemplate::ChatML => "chatml",
             PromptTemplate::Llama2 => "llama2",
             PromptTemplate::Llama3 => "llama3",
+            PromptTemplate::Custom(_) => "custom",
         }
     }
     pub fn messages_to_tokens(&self, model: &llama_cpp::LlamaModel, prompt: &[Message]) -> Result<Vec<Token>, LlamaTokenizationError> {
@@ -39,6 +50,7 @@ impl PromptTemplate {
             Self::ChatML => Self::tokens_chatml(&mut tokens, model, prompt),
             Self::Llama2 => Self::tokens_llama2(&mut tokens, model, prompt),
             Self::Llama3 => Self::tokens_llama3(&mut tokens, model, prompt),
+            Self::Custom (custom_template) => Self::tokens_custom(&mut tokens, model, prompt, custom_template),
         }?;
         Ok(tokens)
     }
@@ -112,6 +124,24 @@ impl PromptTemplate {
         append_to_vec(tokens, &[end_header_id, nl, nl]);
         Ok(())
     }
+    pub fn tokens_custom(tokens: &mut Vec<Token>, model: &llama_cpp::LlamaModel, prompt: &[Message], custom_template: &CustomTemplate) -> Result<(), LlamaTokenizationError> {
+        let system_prefix_tokens = model.tokenize_bytes(&custom_template.system_prefix, false, true)?;
+        let system_suffix_tokens = model.tokenize_bytes(&custom_template.system_suffix, false, true)?;
+        let user_prefix_tokens = model.tokenize_bytes(&custom_template.user_prefix, false, true)?;
+        let user_suffix_tokens = model.tokenize_bytes(&custom_template.user_suffix, false, true)?;
+        let assistant_prefix_tokens = model.tokenize_bytes(&custom_template.assistant_prefix, false, true)?;
+        let assistant_suffix_tokens = model.tokenize_bytes(&custom_template.assistant_suffix, false, true)?;
+        prompt.iter()
+            .for_each(|m| {
+                let content_tokens = model.tokenize_bytes(&m.content, false, false).unwrap();
+                match m.role {
+                    Role::System => vec_merge!(tokens, &system_prefix_tokens, &content_tokens, &system_suffix_tokens),
+                    Role::User => vec_merge!(tokens, &user_prefix_tokens, &content_tokens, &user_suffix_tokens),
+                    Role::Assistant => vec_merge!(tokens, &assistant_prefix_tokens, &content_tokens, &assistant_suffix_tokens),
+                }
+            });
+        Ok(())
+    }
     pub fn stop_tokens(&self, model: &llama_cpp::LlamaModel) -> Result<Vec<Token>, LlamaTokenizationError> {
         match self {
             PromptTemplate::ChatML => {
@@ -126,6 +156,7 @@ impl PromptTemplate {
                 let eot_id = model.tokenize_bytes("<|eot_id|>", false, true)?.first().copied().unwrap();
                 Ok(vec![eot_id, model.eos()])
             },
+            PromptTemplate::Custom(custom_template) => Ok(vec![model.tokenize_bytes(&custom_template.user_prefix, false, true)?.first().copied().unwrap()]),
         }
     }
 }
