9368e7380c2baa6b04a229b5dbf90f6055bfbc7a
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-05-03 11:12:08 +0200

    macro to easily create hashmap

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,3 +3,13 @@ pub mod flatten_stream;
 
 pub use split_bytes::{SplitBytes, SplitBytesFactory};
 pub use flatten_stream::FlattenTrait;
+
+macro_rules! hashmap {
+    ($($name:ident => $value:expr),*) => {{
+        let mut map = ::std::collections::HashMap::new();
+        $(map.insert({stringify!($name)}.into(), {$value}.into());)*
+        map
+    }};
+}
+
+pub(crate) use hashmap;
