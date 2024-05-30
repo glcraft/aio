2ae14d2bf8e18b2b633c2c6e7708182e1b6d3e45
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-05-31 00:04:36 +0200

    new api key retreiver

--- src/credentials.rs
+++ src/credentials.rs
@@ -1,14 +1,20 @@
+use std::collections::HashMap;
 use serde::Deserialize;
-use crate::{
-    serde_io::DeserializeExt,
-    generators::openai::credentials::Credentials as CredsOpenAI
-};
+use crate::serde_io::DeserializeExt;
 
 
 #[derive(Debug, Deserialize)]
 pub struct Credentials {
-    pub openai: CredsOpenAI
+    pub apikey: HashMap<String, String>
 }
 
 impl DeserializeExt for Credentials {}
 
+impl Credentials {
+    pub fn get_api_key(&self, name: &str, env_var: Option<&str>) -> Option<String> {
+        self.apikey
+            .get(name)
+            .cloned()
+            .or_else(|| env_var.and_then(|v| std::env::var(v).ok()))
+    }
+}
